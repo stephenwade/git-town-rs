8104730fa319d19b7c03358988923380cd3ba0e0
Stephen Wade <stephen@stephenwade.me>
2025-06-07 21:12:04 -0400

    Figure out how to get the Git version

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use std::process::Command;
+
 fn main() {
-    println!("Hello, world!");
+    // First step: Figure out how to call another command.
+
+    // I want to run `git --version` and output the result.
+
+    let git_version_output = Command::new("git").arg("--version").output().unwrap();
+
+    print!("{}", str::from_utf8(&git_version_output.stdout).unwrap())
 }
